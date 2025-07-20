2cc5f9e973a9bbb74fde7979e2091ccdbf49f73a
Annada Behera <annada.behera@hotmail.com>
2025-07-20 14:21:27 +0530

    Added package script to install required packages

---
+++ install_packages.sh
@@ -0,0 +1,6 @@
+xbps-install -S
+xbps-install -uy xbps
+xbps-install -yu void-repo-nonfree
+xbps-install -S
+xbps-install -Suy fish-shell 7zip kakoune kak-lsp curl wget ugrep bat qalculate elogind polkit dbus vivaldi xtools inotify-tools xmirror
+
