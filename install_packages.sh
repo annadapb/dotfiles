e56428620924e3af736a51ba57f4e5441f63dd6a
Annada Behera <annada.behera@hotmail.com>
2025-07-21 12:25:17 +0530

    Added more required pacakges

--- install_packages.sh
+++ install_packages.sh
@@ -2,5 +2,4 @@ xbps-install -S
 xbps-install -uy xbps
 xbps-install -yu void-repo-nonfree
 xbps-install -S
-xbps-install -Suy fish-shell 7zip kakoune kak-lsp curl wget ugrep bat qalculate elogind polkit dbus vivaldi xtools inotify-tools xmirror
-
+xbps-install -Suy fish-shell 7zip kakoune kak-lsp curl wget ugrep bat qalculate elogind polkit dbus vivaldi xtools inotify-tools xmirror tree fuse-sshfs ImageMagick zathura zathura{,-pdf-mupdf,-ps} bc par pluma caja-open-terminal
