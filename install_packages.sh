fd4cb788f8aeabb3c0c5d5671e63839854f93e27
Annada Behera <annada.behera@hotmail.com>
2026-01-15 10:21:14 +0530

    Removed the brace-expansion for package names

--- install_packages.sh
+++ install_packages.sh
@@ -2,4 +2,4 @@ xbps-install -S
 xbps-install -uy xbps
 xbps-install -yu void-repo-nonfree
 xbps-install -S
-xbps-install -Suy fish-shell 7zip kakoune kak-lsp curl wget ugrep bat qalculate elogind polkit dbus vivaldi xtools inotify-tools xmirror tree fuse-sshfs ImageMagick zathura zathura{,-pdf-mupdf,-ps} bc par pluma caja-open-terminal
+xbps-install -Suy fish-shell 7zip kakoune kak-lsp curl wget ugrep bat qalculate elogind polkit dbus xtools inotify-tools xmirror tree fuse-sshfs ImageMagick zathura zathura-pdf-mupdf zathura-ps bc par pluma caja-open-terminal
